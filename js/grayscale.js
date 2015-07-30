5963b2051c14b1d645f6215408b537c399296883
Alberto Barradas <abcsds@gmail.com>
2015-07-29 19:46:16 -0500

    Zoom mat into oldcity

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,7 +39,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 13,
+        zoom: 7,
 
         // The latitude and longitude to center the map (always required)
         // center: {lat: 19.4310, lng: -99.1296},
